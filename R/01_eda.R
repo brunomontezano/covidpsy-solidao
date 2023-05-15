53c6f7585ee498a477a552355c5e9554e7170a5a
Bruno Montezano <brunodoyt@gmail.com>
2023-05-15 00:01:24 -0300

    gerar análise descritiva de respostas válidas para cada combinação de waves.

--- R/01_eda.R
+++ R/01_eda.R
@@ -74,3 +74,53 @@ trajetoria_ucla_w1_w4 |>
     title = "Na onda 1 e 4, temos dados da UCLA de 944 sujeitos.",
     subtitle = "O gráfico mostra o número de sujeitos com cada trajetória de solidão."
   )
+
+# Ler dados brutos para ver possíveis análises com UCLA
+dados_brutos <- readr::read_rds("data/processed/dados_brutos.rds")
+
+dados_brutos |>
+  dplyr::select(
+    w1 = ucla_1,
+    w2 = ucla_1_w2,
+    w3 = ucla_1_w3,
+    w4 = ucla_1_w4
+  ) |>
+  dplyr::mutate(
+    dplyr::across(dplyr::everything(),
+                  \(x) dplyr::case_when(
+                    is.na(x) ~ "i",
+                    .default = "v"
+                  )),
+    como_analisar = dplyr::case_when(
+      w1 == "v" & w2 == "v" & w3 == "v" & w4 == "v" ~ "W1 + W2 + W3 + W4",
+      w1 == "v" & w2 == "i" & w3 == "i" & w4 == "i" ~ "W1",
+      w1 == "v" & w2 == "v" & w3 == "v" & w4 == "i" ~ "W1 + W2 + W3",
+      w1 == "v" & w2 == "v" & w3 == "i" & w4 == "v" ~ "W1 + W2 + W4",
+      w1 == "v" & w2 == "i" & w3 == "v" & w4 == "v" ~ "W1 + W3 + W4",
+      w1 == "v" & w2 == "i" & w3 == "i" & w4 == "v" ~ "W1 + W4",
+      w1 == "v" & w2 == "v" & w3 == "i" & w4 == "i" ~ "W1 + W2",
+      w1 == "v" & w2 == "i" & w3 == "v" & w4 == "i" ~ "W1 + W3",
+    )
+  ) |>
+  dplyr::count(como_analisar) |>
+  dplyr::filter(como_analisar != "W1") |>
+  dplyr::mutate(
+    como_analisar = forcats::fct_reorder(como_analisar, n)
+  ) |>
+  ggplot2::ggplot(ggplot2::aes(
+    x = n,
+    y = como_analisar,
+    label = n
+  )) +
+  ggplot2::geom_col(fill = "coral") +
+  ggplot2::geom_text(
+    position = ggplot2::position_stack(vjust = 0.5),
+    size = 6,
+    family = "Charter"
+  ) +
+  ggplot2::theme_classic(16, "Charter") +
+  ggplot2::labs(x = "# de sujeitos",
+                y = NULL,
+                title = "Respostas válidas na UCLA em cada combinação de ondas.",
+                subtitle = "Para calcular, por exemplo, W1 + W4: 390 + 270 + 157 + 127 = 944 sujeitos.",
+                caption = "Fonte: COVIDPsy.")
